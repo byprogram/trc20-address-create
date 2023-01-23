c2ad993db234a8b3b884b9ab794ccc71777b7e55
byprogram <121013897+byprogram@users.noreply.github.com>
2023-01-24 02:11:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const {Worker, isMainThread} = require('worker_threads');
-var xiancheng = 10; //线程数量
+var xiancheng = 5; //线程数量
 
 console.log("启动成功，正在生成靓号地址，请不要关闭此窗口！")
 for (let index = 0; index < xiancheng; index++) {
